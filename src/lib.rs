c4e27429b7814ebe9886e2c85295eb4f2557854e
mitchmindtree <mail@mitchellnordine.com>
2023-09-07 00:12:50 +1000

    Fix whole spans within timecat pattern events

--- src/lib.rs
+++ src/lib.rs
@@ -902,7 +902,12 @@ where
                 .filter_map(move |i| {
                     let (p_span, pattern) = &ps[i];
                     let p_span = p_span.map(|r| r + sam);
-                    cycle.intersect(p_span).map(|sect| pattern.query(sect))
+                    cycle.intersect(p_span).map(|sect| {
+                        pattern.query(sect).map(move |mut ev| {
+                            ev.span.whole = Some(p_span);
+                            ev
+                        })
+                    })
                 })
                 .flatten()
         })
@@ -1193,19 +1198,19 @@ fn test_timecat() {
         .map(|ev| (ev.value, ev.span.active, ev.span.whole));
     assert_eq!(
         es.next(),
-        Some(("a", span!(1 / 4, 1 / 3), Some(span!(0 / 1, 1 / 1)))),
+        Some(("a", span!(1 / 4, 1 / 3), Some(span!(0 / 1, 1 / 3)))),
     );
     assert_eq!(
         es.next(),
-        Some(("b", span!(1 / 3, 1 / 1), Some(span!(0 / 1, 1 / 1)))),
+        Some(("b", span!(1 / 3, 1 / 1), Some(span!(1 / 3, 1 / 1)))),
     );
     assert_eq!(
         es.next(),
-        Some(("a", span!(1 / 1, 4 / 3), Some(span!(1 / 1, 2 / 1)))),
+        Some(("a", span!(1 / 1, 4 / 3), Some(span!(1 / 1, 4 / 3)))),
     );
     assert_eq!(
         es.next(),
-        Some(("b", span!(4 / 3, 3 / 2), Some(span!(1 / 1, 2 / 1)))),
+        Some(("b", span!(4 / 3, 3 / 2), Some(span!(4 / 3, 2 / 1)))),
     );
     assert_eq!(es.next(), None);
 }
